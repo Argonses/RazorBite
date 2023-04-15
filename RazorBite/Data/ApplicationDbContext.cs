432d10c121679ccb390fd3510c7751f82fd5a71d
resr <vaclavsr@post.cz>
2023-04-15 08:45:03 +0200

    Fixed bugs

--- RazorBite/Data/ApplicationDbContext.cs
+++ RazorBite/Data/ApplicationDbContext.cs
@@ -1,12 +1,14 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using RazorBite.Models;
+using System.Reflection.Emit;
 
 namespace RazorBite.Data
 {
-	public class ApplicationDbContext : IdentityDbContext
+	public class ApplicationDbContext : IdentityDbContext<IdentityUser>
 	{
-		public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
-			: base(options)
+		public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
 		{
 		}
 
--- RazorBite/Migrations/20230415063902_IdentityData.Designer.cs
+++ RazorBite/Migrations/20230415063902_IdentityData.Designer.cs
@@ -3,17 +3,20 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using RazorBite.Data;
 
 #nullable disable
 
-namespace RazorBite.Data.Migrations
+namespace RazorBite.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230415063902_IdentityData")]
+    partial class IdentityData
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
